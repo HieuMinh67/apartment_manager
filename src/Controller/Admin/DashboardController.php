a16a8ef6c48497e1d91600fb2402a2816ea018c8
HieuMinh67 <minhhieu060799@gmail.com>
2020-10-29 07:49:52 +0700

    Add change password

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,27 +3,49 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Apartment;
+use App\Entity\Building;
 use App\Entity\Citizen;
+use App\Entity\Quotation;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
-use EasyCorp\Bundle\EasyAdminBundle\Router\CrudUrlGenerator;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class DashboardController extends AbstractDashboardController
 {
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     /**
      * @Route("/admin", name="admin")
      */
     public function index(): Response
     {
-//        $routeBuilder = $this->get(CrudUrlGenerator::class)->build();
-//
-//        return $this->redirect($routeBuilder->setController(ApartmentCrudController::class)->generateUrl());
-//        return parent::index();
         return $this->render('bundles/EasyAdminBundle/page/dashboard.html.twig');
+    }
+
+    /**
+     * @Route("/password/change", name="change_password", methods={"GET","POST"})
+     */
+    public function change_password(Request $request): Response
+    {
+        $user = $this->getUser();
+        if ($request->isMethod('POST')) {
+            $form = $request->request->all();
+            if ($form['_new_password'] === $form['_retype_new_password'] && strcmp($this->encoder->encodePassword($user, $form['_old_password']), $user->getPassword())) {
+                $this->getDoctrine()->getRepository(User::class)->upgradePassword($user, $this->encoder->encodePassword($user, 'admin'));
+                return $this->redirectToRoute('app_logout');
+            }
+        }
+        return $this->render('bundles/EasyAdminBundle/page/_change_password_form.html.twig');
     }
 
     public function configureDashboard(): Dashboard
@@ -43,5 +65,10 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToCrud('Add citizen', 'fa fa-plus', Citizen::class)->setAction('new');
         yield MenuItem::section("Apartment");
         yield MenuItem::linkToCrud('List apartment', 'fa fa-home', Apartment::class);
+        yield MenuItem::section("Building");
+        yield MenuItem::linkToCrud('List building', 'fa fa-building', Building::class);
+        yield MenuItem::linkToCrud('Add building', 'fa fa-plus', Building::class)->setAction('new');
+        yield MenuItem::section("Quote request");
+        yield MenuItem::linkToCrud('List quote', 'fa fa-envelope', Quotation::class);
     }
 }
---
+++ src/Controller/LandingpageController.php
@@ -0,0 +1,61 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Entity\Building;
+use App\Entity\Citizen;
+use App\Entity\Quotation;
+use App\Entity\User;
+use App\Form\QuotationType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LandingpageController extends AbstractController
+{
+
+
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function homepage(): Response {
+        $buildings = $this->getDoctrine()->getRepository(Building::class)->findAll();
+        $employee = $this->getDoctrine()->getRepository(User::class)->countEmployee();
+        $quote = $this->getDoctrine()->getRepository(Quotation::class)->countQuote();
+        $citizen = $this->getDoctrine()->getRepository(Citizen::class)->countCitizen();
+        $form = $this->createForm(QuotationType::class);
+        return $this->render('LandingPage/index.html.twig', ["buildings" => $buildings, "numberOfCitizen" => $citizen, "numberOfQuote" => $quote, "numberOfBuildings" => count($buildings), "numberOfEmployee" => $employee, "quoteForm" => $form->createView()]);
+    }
+
+    /**
+     * @Route("about", name="about")
+     */
+    public function about(): Response {
+        $buildings = $this->getDoctrine()->getRepository(Building::class)->findAll();
+        $form = $this->createForm(QuotationType::class);
+        return $this->render('LandingPAge/about.html.twig', ["buildings" => $buildings, "quoteForm" => $form->createView()]);
+    }
+
+    /**
+     * @Route("contact", name="contact")
+     */
+    public function contact(): Response {
+        return $this->render("LandingPage/contact.html");
+    }
+
+    /**
+     * @Route("services", name="service")
+     */
+    public function service(): Response {
+        return $this->render("LandingPage/services.html");
+    }
+
+    /**
+     * @Route("projects", name="project")
+     */
+    public function project(): Response {
+        return $this->render("LandingPage/projects.html");
+    }
+}
