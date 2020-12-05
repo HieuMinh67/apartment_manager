9d0806f83502b9ee93d2eb2f106d1472e35c859c
HieuMinh67 <minhhieu060799@gmail.com>
2020-12-05 10:37:36 +0700

    update DataFixtures

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -4,20 +4,24 @@
 namespace App\DataFixtures;
 
 
+use App\Entity\Apartment;
 use App\Entity\Citizen;
 use App\Entity\Employee;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixtures extends Fixture
 {
     private $encoder;
+    private $repo;
 
-    public function __construct(UserPasswordEncoderInterface $encoder)
+    public function __construct(UserPasswordEncoderInterface $encoder, EntityManagerInterface $em)
     {
         $this->encoder = $encoder;
+        $this->repo = $em->getRepository(Apartment::class);
     }
 
     public function load(ObjectManager $manager)
@@ -28,7 +32,7 @@ class UserFixtures extends Fixture
         $email = ["admin@novaland.com", 'manager1@novaland.com', 'manager2@novaland.com', 'staff1@novaland.com', 'staff2@novaland.com', 'staff3@novaland.com'];
         for ($i = 0; $i < 6; $i++) {
             $employee = new Employee();
-            $employee->setPhone("+84989190190");
+            $employee->setPhone("+849".strval(rand(10000000,99999999)));
             $employee->setFirstName($firstName[$i]);
             $employee->setLastName($lastName[$i]);
             $user = new User();
@@ -37,7 +41,6 @@ class UserFixtures extends Fixture
             $user->setEmployee($employee);
             $manager->persist($user);
             $manager->persist($employee);
-            $manager->flus();
         }
 
         $csv = fopen(dirname(__FILE__) . '/data.csv', 'r');
@@ -45,6 +48,7 @@ class UserFixtures extends Fixture
         $gender = ['female', 'male'];
         $min = strtotime('85 years ago');
         $max = strtotime('5 years ago');
+        $apartments = $this->repo->findAll();
         while ($line = fgetcsv($csv, 203, "|")) {
             $rand_time = mt_rand($min, $max);
             $citizen[$i] = new Citizen();
@@ -53,8 +57,7 @@ class UserFixtures extends Fixture
             $citizen[$i]->setPhone($line[0]);
             $citizen[$i]->setGender(array_rand($gender, 1));
             $citizen[$i]->setDateOfBirth(\DateTime::createFromFormat('Y-m-d', date('Y-m-d', $rand_time)));
-//            $citizen[$i]->set
-//            $citizen[$i]->setApartmentId('');
+            $citizen[$i]->setApartmentId($apartments[array_rand($apartments, 1)]);
             $manager->persist($citizen[$i]);
 
             $this->addReference("new Citizen-" . $i, $citizen[$i]);
